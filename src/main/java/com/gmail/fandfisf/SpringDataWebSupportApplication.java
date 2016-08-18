5c3b3fe1b7773ae5b4efe08fb79a25734b571be0
Prashant Khanwale <pskhanwale@gmail.com>
2016-08-17 19:56:42 -0700

    Initial commit

---
+++ src/main/java/com/gmail/fandfisf/SpringDataWebSupportApplication.java
@@ -0,0 +1,12 @@
+package com.gmail.fandfisf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDataWebSupportApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDataWebSupportApplication.class, args);
+	}
+}
